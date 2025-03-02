965bcd752798c7f513a5652c1f5e2855bb6a7cf5
GEORGIAN86 <sumitgeorgian3986@gmail.com>
2025-03-02 15:28:40 +0530

    My changes after cloning

--- pretrain/launch.sh
+++ pretrain/launch.sh
@@ -1,5 +1,5 @@
 export OMP_NUM_THREADS=8
 
 wandb online
-cd /your_path/MERL/pretrain
+cd /media/sumit/dd6174bf-2d05-4a68-9324-d66b0a8e63762/Aditi/MERL-ICML2024/pretrain
 torchrun --nnodes=1 --nproc_per_node=8 --rdzv_id=101 --rdzv_endpoint=localhost:29502 main.py
--- pretrain/main.py
+++ pretrain/main.py
@@ -18,87 +18,82 @@ import utils_builder
 
 import wandb
 
-
 os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
-
 def ddp_main():
-    dist.init_process_group("nccl")
-    torch.cuda.empty_cache()
+    # Initialize the process group with Gloo backend for CPU
+    dist.init_process_group("gloo")
+    
     rank = dist.get_rank()
-
     print(f"Start running basic DDP example on rank {rank}.")
-    device_id = rank % torch.cuda.device_count()
 
-    # set up
+    # Use CPU as device
+    device = torch.device("cpu")
+
+    # Load configuration
     config = yaml.load(open("config.yaml", "r"), Loader=yaml.FullLoader)
 
-    if device_id == 0:
+    # Initialize Weights & Biases logging (only for rank 0)
+    if rank == 0:
         run = wandb.init(
-            # Set the project where this run will be logged
             project="MERL_ICML",
-            name = config['wandb_name'],
-            # Track hyperparameters and run metadata
+            name=config['wandb_name'],
             config={
-                    "learning_rate": config['optimizer']['params']['lr'],
-                    "total_epochs": config['trainer']['max_epochs'],
-                    'weight_decay': config['optimizer']['params']['weight_decay'],
-                    'ecg_model': config['network']['ecg_model'],
-                    'text_model': config['network']['text_model'],
-                    'batch_size': config['trainer']['batch_size'],
-                    'val_zeroshot': 'all_sets',
-                    'prompt_type': config['zeroshot']['prompt_type'],
+                "learning_rate": config['optimizer']['params']['lr'],
+                "total_epochs": config['trainer']['max_epochs'],
+                'weight_decay': config['optimizer']['params']['weight_decay'],
+                'ecg_model': config['network']['ecg_model'],
+                'text_model': config['network']['text_model'],
+                'batch_size': config['trainer']['batch_size'],
+                'val_zeroshot': 'all_sets',
+                'prompt_type': config['zeroshot']['prompt_type'],
             }
         )
 
+    # Set seeds for reproducibility
     torch.manual_seed(42)
     random.seed(0)
     np.random.seed(0)
-    # loading data path
+
+    # Load dataset path from config
     data_path = config['dataset']['data_path']
 
-    # define image-text dataset
+    # Define ECG-Text dataset
     dataset = utils_dataset.ECG_TEXT_Dsataset(
-        data_path=data_path, dataset_name=config['dataset']['dataset_name'])
+        data_path=data_path, dataset_name=config['dataset']['dataset_name']
+    )
     train_dataset = dataset.get_dataset(train_test='train')
     val_dataset = dataset.get_dataset(train_test='val')
 
-    # building model part
-    # --------------------
+    # Build model
     model = utils_builder.ECGCLIP(config['network'])
-    
-    '''
-    you can freeze bert from last layer to first layer.
-    set num of layer in config.yaml
-    default is freeze 9 layers
-    '''
+
+    # Freeze BERT layers if specified
     if config['network']['free_layers'] is not None:
         for layer_idx in range(int(config['network']['free_layers'])):
             for param in list(model.lm_model.encoder.layer[layer_idx].parameters()):
                 param.requires_grad = False
 
-    model = model.to(device_id)
-    model = DDP(model, device_ids=[device_id], find_unused_parameters=True)
+    model = model.to(device)
+    model = DDP(model, device_ids=None, find_unused_parameters=True)  # CPU Mode
 
-    # --------------------
-
-    # --------------------
+    # Define optimizer
     optimizer = torch.optim.AdamW(
         model.parameters(),
         **config['optimizer']['params'],
         betas=(0.9, 0.999)
     )
 
-    # ---------xw-----------
-    trainer = trainer_wBert(model=model,
-                            optimizer=optimizer,
-                            device=rank,
-                            model_name=config['wandb_name'],
-                            **config['trainer'])
-    # --------------------
-    
-    # --------------------
-    # I_T_P_trainer
+    # Initialize trainer
+    trainer = trainer_wBert(
+        model=model,
+        optimizer=optimizer,
+        device=rank,
+        model_name=config['wandb_name'],
+        **config['trainer']
+    )
+
+    # Train the model
     trainer.train_w_TextEmb(train_dataset, val_dataset, config['zeroshot'])
 
 
